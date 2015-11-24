39b9f23e166d40ab1f7702b11f698940e9759ab9
Ademola Diamond <diamonddemola@yahoo.co.uk>
2015-11-24 18:41:59 +0000

    share alarm

---
+++ app/src/main/java/ml/mk/jm/ay/ak/studenttoolkit/todo/AlertReceiver.java
@@ -0,0 +1,46 @@
+package ml.mk.jm.ay.ak.studenttoolkit.todo;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import ml.mk.jm.ay.ak.studenttoolkit.R;
+import ml.mk.jm.ay.ak.studenttoolkit.database.DatabaseConnection;
+
+/**
+ * Created by Forbes Diamond on 11/24/2015.
+ */
+public class AlertReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        int todoId = intent.getIntExtra("TODO_ID", -1);
+        String todoTitle =  intent.getStringExtra("TODO_TITLE");
+        String todoDesc =  intent.getStringExtra("TODO_DESCRIPTION");
+        Log.d("todo", "todo Id: " + todoId + " todoTitle: " + todoTitle + " todoDes" + todoDesc);
+
+        //createNotification(context, "Times up", "5 minutes has passed", "Alert", todoId);
+        createNotification(context, todoTitle, todoDesc, "Todo Alert", todoId);
+    }
+
+    private void createNotification(Context context, String msg, String msgText, String msgAlert, int todoId) {
+        PendingIntent notifyIntent = PendingIntent.getActivity(context, 0, new Intent(context, NewToDoActivity.class), 0);
+        NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.add)
+                .setContentTitle(msg)
+                .setContentText(msgText)
+                .setTicker(msgAlert);
+        nBuilder.setContentIntent(notifyIntent);
+        //nBuilder.setDefaults(NotificationCompat.DEFAULT_SOUND);
+        nBuilder.setDefaults(NotificationCompat.DEFAULT_ALL);
+        nBuilder.setAutoCancel(true);
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+        notificationManager.notify(todoId, nBuilder.build());
+
+
+    }
+}
